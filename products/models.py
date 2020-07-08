84d112acaf188841bcd03df353d42f343922a7df
Beppo <beppo@localhost.localdomain>
2020-07-08 12:24:15 -0500

    Agregada app Users y AbstractUser

--- products/models.py
+++ products/models.py
@@ -23,11 +23,11 @@ def set_slug(sender, instance, *args, **kwargs): #callback
     if instance.title and not instance.slug:
         slug = slugify(instance.title)
 
-    while Product.objects.filter(slug=slug).exists():
-        slug = slugify(
-            '{}-{}'.format(instance.title, str(uuid.uuid4())[:8] )
-        )
+        while Product.objects.filter(slug=slug).exists():
+            slug = slugify(
+                '{}-{}'.format(instance.title, str(uuid.uuid4())[:8] )
+            )
 
-    instance.slug = slug
+        instance.slug = slug
 
 pre_save.connect(set_slug, sender=Product)
